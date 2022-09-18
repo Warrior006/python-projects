8c2f87ce314bf0f536097999cb460dff0e081381
Farouk EL MAZI <98127521+Warrior006@users.noreply.github.com>
2022-09-18 23:59:19 +0200

    Add files via upload

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,14 +2,13 @@ import random
 
 
 def play():
-    user = input("What is your choice? 'r' for rock, 'p' for paper, and 's' for scissors\n")
-
-    while (user != 'r') or (user != 'p') or (user != 's'):
-        user = str(input("Please choose 'r' for rock, 'p' for paper, and 's' for scissors\n"))
-        if (user == 'r') or (user == 'p') or (user == 's'):
-            break
 
     computer = random.choice(['r', 'p', 's'])
+
+    while True:
+        user = input("Please choose 'r' for rock, 'p' for paper, and 's' for scissors\n")
+        if (user == 'r') or (user == 'p') or (user == 's'):
+            break
 
     print(f"You chose {user}, and the computer chose {computer}.")
 
@@ -18,7 +17,7 @@ def play():
 
     # r > s , p > r, s > p
     if is_win(user, computer):
-        return "You won! BABAAAY"
+        return "You won!"
 
     return "You lost!"
 
